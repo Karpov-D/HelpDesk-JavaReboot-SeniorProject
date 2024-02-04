8e55ca7356fc610e1b24ebe1df06dccad495dfc8
Karpov-D <karpov0910@mail.ru>
2024-02-04 07:26:18 +0300

    Добавлены классы конфиги

---
+++ src/main/java/ru/edu/config/SecurityConfig.java
@@ -0,0 +1,14 @@
+package ru.edu.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+public class SecurityConfig {
+
+
+}
