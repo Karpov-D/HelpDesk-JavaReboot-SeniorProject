749a6ee909003dac6629a012980aef6d5d7ad351
Dmitry <karpov0910@mail.ru>
2024-02-07 05:14:59 +0300

    Добавлена стратовая страница подключен css

--- src/main/java/ru/edu/controller/WelcomeController.java
+++ src/main/java/ru/edu/controller/WelcomeController.java
@@ -3,14 +3,11 @@ package ru.edu.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import ru.edu.service.HelpDeskService;
 
 
 @Controller
@@ -18,9 +15,6 @@ import ru.edu.service.HelpDeskService;
 @RequiredArgsConstructor
 @Tag(name = "welcome", description = "Welcome API")
 public class WelcomeController {
-
-    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
-    private final HelpDeskService service;
 
     @GetMapping
     @Operation(summary = "Get welcome page")
